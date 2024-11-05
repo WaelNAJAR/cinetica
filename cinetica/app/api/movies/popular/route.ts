dd6e94c8ff1331ddf2423df67558371f465e1ffd
waelNAJAR <waelnajar408@gmail.com>
2024-11-05 17:27:16 +0100

    initial commit

---
+++ cinetica/app/api/movies/popular/route.ts
@@ -0,0 +1,27 @@
+// app/api/movies/popular.js
+
+export async function GET() {
+  const apiKey = process.env.TMDB_API_KEY;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json'
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(`Erreur de requête : ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    console.error("Erreur lors de la récupération des films populaires ");
+    return new Response(JSON.stringify({ error: "Échec de la récupération des films populaires" }), { status: 500 });
+  }
+}
