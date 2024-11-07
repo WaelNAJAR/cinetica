cd56b15c11e7417314d5ddf87cd4bf866fdd171d
zeinebBoujmil <zeinebboujmil253@gmail.com>
2024-11-07 14:35:00 +0100

    Reapply "11:34"
    This reverts commit 5c5d4626bea936d56d221245129a2359e6e421ce.

---
+++ cinetica/app/components/MovieCard.tsx
@@ -0,0 +1,27 @@
+// components/MovieCard.tsx
+import React from 'react';
+import { Movie } from '../entities/Movie';
+
+interface MovieCardProps {
+    movie: Movie;
+    onClick?: () => void; // Optionnel : pour gérer un clic sur la carte
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ movie, onClick }) => {
+    //console.log("Film reçu dans MovieCard :", movie); // Ajout de console.log pour vérifier les données du film
+
+    return (
+        <>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-xs mx-auto" onClick={onClick}>
+            <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className="w-full h-48 object-cover" />
+            <div className="p-4">
+                <h3 className="text-lg font-semibold text-gray-800 ">{movie.title}</h3>
+                <p className="text-sm text-gray-600">{movie.release_date}</p>
+                <p className="text-sm text-gray-600">{movie.vote_average} / 10</p>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default MovieCard;
---
+++ cinetica/app/components/SerieCard.tsx
@@ -0,0 +1,26 @@
+// components/MovieCard.tsx
+import React from 'react';
+import { TVShow } from '../entities/TVShow';
+interface MovieCardProps {
+    serie: TVShow;
+    onClick?: () => void; // Optionnel : pour gérer un clic sur la carte
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ serie, onClick }) => {
+    //console.log("Film reçu dans MovieCard :", serie); // Ajout de console.log pour vérifier les données du film
+
+    return (
+        <>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-xs mx-auto" onClick={onClick}>
+            <img src={`https://image.tmdb.org/t/p/w300${serie.poster_path}`} alt={serie.name} className="w-full h-48 object-cover" />
+            <div className="p-4">
+                <h3 className="text-lg font-semibold text-gray-800 ">{serie.name}</h3>
+                <p className="text-sm text-gray-600">{serie.first_air_date}</p>
+                <p className="text-sm text-gray-600">{serie.vote_average} / 10</p>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default MovieCard;
